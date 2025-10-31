e3f63821afdae7b078bb2c0d9b7a544ea2ccbf93
julianab16 <julianamelis13@gmail.com>
2025-10-31 16:47:24 -0500

    created models

--- l_atelier/urls.py
+++ l_atelier/urls.py
@@ -15,8 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+from l_atelier.views import (
+    UserViewSet,
+    RegisterView,
+    CustomTokenObtainPairView,
+    MeView,
+)
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/auth/register/', RegisterView.as_view(), name='register'),
+    path('api/auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/me/', MeView.as_view(), name='me'),
 ]
